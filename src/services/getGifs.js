216d1eb1d4924d513a42f26680bb8e7b11236ca1
Sergio Dev <sergio-dev@outlook.com>
2022-04-09 13:58:24 +0200

    Refactor: prettier changes

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,17 +1,17 @@
 const api_key = 'VFEJhtLSFk9qZdOXMUqoWYvAzsv8ZMVM'
 
-export default function getGifs ({keyword = 'lamp'} = {}) {
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=5&offset=0&rating=g&lang=en`
+export default function getGifs({ keyword = 'lamp' } = {}) {
+  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
 
   return fetch(apiUrl)
-    .then(res => res.json())
-    .then(response => {
-      const {data} = response
-      const gifs = data.map(image => {
+    .then((res) => res.json())
+    .then((response) => {
+      const { data } = response
+      const gifs = data.map((image) => {
         const { images, title, id } = image
         const { url } = images.downsized_medium
-        return { title, id, url } 
+        return { title, id, url }
       })
       return gifs
-  })
+    })
 }
