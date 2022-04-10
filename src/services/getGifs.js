d773b1e282a592f8a12d412fd1bd5592d5ad1ff7
Sergio Dev <sergio-dev@outlook.com>
2022-04-10 20:43:18 +0200

    Feat: improve api data manipulation

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,17 +1,21 @@
-const api_key = 'VFEJhtLSFk9qZdOXMUqoWYvAzsv8ZMVM'
+import { API_KEY, API_URL } from './settings'
 
-export default function getGifs({ keyword = 'lamp' } = {}) {
-  const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${keyword}&limit=10&offset=0&rating=g&lang=en`
+const fromApiResponseToGifs = (apiResponse) => {
+  const { data = [] } = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map((image) => {
+      const { images, title, id } = image
+      const { url } = images.downsized_medium
+      return { title, id, url }
+    })
+    return gifs
+  }
+  return []
+}
 
+export default function getGifs({ keyword = 'lamp', limit = 25 } = {}) {
+  const apiUrl = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=g&lang=en`
   return fetch(apiUrl)
     .then((res) => res.json())
-    .then((response) => {
-      const { data } = response
-      const gifs = data.map((image) => {
-        const { images, title, id } = image
-        const { url } = images.downsized_medium
-        return { title, id, url }
-      })
-      return gifs
-    })
+    .then(fromApiResponseToGifs)
 }
