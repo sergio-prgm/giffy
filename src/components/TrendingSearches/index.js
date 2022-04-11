404f71e5b2fd612df858c85839c7513109208e27
Sergio Dev <sergio-dev@outlook.com>
2022-04-11 10:03:44 +0200

    Feat: lazy load trending categories

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import getTrending from 'services/getTrendingService'
 import Category from 'components/Category/index'
 
-export default function TrendingSearches() {
+function TrendingSearches() {
   const [trends, setTrends] = useState([])
 
   useEffect(function () {
@@ -10,4 +10,22 @@ export default function TrendingSearches() {
   }, [])
 
   return <Category categoryName="trending" options={trends} />
+}
+
+export default function LazyTrending() {
+  const [show, setShow] = useState(false)
+
+  useEffect(function () {
+    const onChange = (entries) => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setShow(true)
+      }
+    }
+    const observer = new IntersectionObserver(onChange, {
+      rootMargin: '100px'
+    })
+    observer.observe(document.getElementById('LazyTrending'))
+  })
+  return <div id="LazyTrending">{show ? <TrendingSearches /> : null}</div>
 }
