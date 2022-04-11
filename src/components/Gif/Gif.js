0624e4ea879d7aab2c311ffa6936c67020617b47
Sergio Dev <sergio-dev@outlook.com>
2022-04-11 13:24:21 +0200

    Feat: add css grid to listOfGifs

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -3,9 +3,11 @@ import './Gif.css'
 
 export default function Gif({ title, id, url }) {
   return (
-    <Link to={`/gif/${id}`} className="gif">
-      <h4>{title}</h4>
-      <img src={url} alt={title} />
-    </Link>
+    <div className="Gif">
+      <Link to={`/gif/${id}`} className="Gif-link">
+        <h4>{title}</h4>
+        <img src={url} alt={title} />
+      </Link>
+    </div>
   )
 }
