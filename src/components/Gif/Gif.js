c3ac3dc50d5994fa69224a8efe4e3370b5e141ce
Sergio Dev <sergio-dev@outlook.com>
2022-04-12 13:10:12 +0200

    Perf: custom memo for Gif

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -1,7 +1,8 @@
+import { memo } from 'react'
 import { Link } from 'wouter'
 import './Gif.css'
 
-export default function Gif({ title, id, url }) {
+function Gif({ title, id, url }) {
   return (
     <div className="Gif">
       <Link to={`/gif/${id}`} className="Gif-link">
@@ -11,3 +12,9 @@ export default function Gif({ title, id, url }) {
     </div>
   )
 }
+
+export default memo(Gif, (prevProps, nextProps) => {
+  return prevProps.id === nextProps.id
+})
+//  memo() doesn't render the component if it returns true (default export would return false)
+// on ListOfGifs if props are passed as an object {...gifProps} it prevents memo to work properly
