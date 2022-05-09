60c3118e01b06abd4896908be9f5638089a7c647
sergio-dev <sergio-dev@outlook.com>
2022-05-09 13:11:15 +0200

    Refactor: refactor useReducer

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -3,21 +3,51 @@ import { useLocation } from 'wouter'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
-const reducer = (state, action) => {
-  if (action.type === 'update_keyword') {
-    return {
-      ...state,
-      keyword: action.payload,
-      times: state.times + 1
-    }
-  } else if (action.type === 'update_rating') {
-    return {
-      ...state,
-      rating: action.payload
-    }
-  }
-  return state
+const ACTIONS = {
+  UPDATE_KEYWORD: 'update_keyword',
+  UPDATE_RATING: 'update_rating'
 }
+
+// Add language filter
+// Add button to clear all filters
+
+const ACTION_REDUCERS = {
+  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
+    ...state,
+    keyword: action.payload,
+    times: state.times + 1
+  }),
+  [ACTIONS.UPDATE_RATING]: (state, action) => ({
+    ...state,
+    rating: action.payload
+  })
+}
+
+const reducer = (state, action) => {
+  const actionReducer = ACTION_REDUCERS[action.type]
+  return actionReducer ? actionReducer(state, action) : state
+}
+
+// Common way of writing the reducer function
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case ACTIONS.UPDATE_KEYWORD:
+//       return {
+//         ...state,
+//         keyword: action.payload,
+//         times: state.times + 1
+//       }
+//
+//     case ACTIONS.UPDATE_RATING:
+//       return {
+//         ...state,
+//         rating: action.payload
+//       }
+//     default:
+//       return state
+//       // throw new Error('Action not supported') // Otra opción para controlar edgecases
+//   }
+// }
 
 function SearchForm ({ initialKeyword = '', initialRating = 'g' }) {
   // const [rating, setRating] = useState(initialRating)
@@ -33,11 +63,11 @@ function SearchForm ({ initialKeyword = '', initialRating = 'g' }) {
   const [, pushLocation] = useLocation()
 
   const handleChange = (evt) => {
-    dispatch({ type: 'update_keyword', payload: evt.target.value })
+    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: evt.target.value })
   }
 
   const handleChangeRating = (evt) => {
-    dispatch({ type: 'update_rating', payload: evt.target.value })
+    dispatch({ type: ACTIONS.UPDATE_RATING, payload: evt.target.value })
   }
 
   const handleSubmit = (evt) => {
