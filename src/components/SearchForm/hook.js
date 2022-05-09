08d4cf70f70c3a67128a2b00b42a048513ce0b2e
sergio-dev <sergio-dev@outlook.com>
2022-05-09 13:32:05 +0200

    Refactor: extract all logic to single local hook

---
+++ src/components/SearchForm/hook.js
@@ -0,0 +1,63 @@
+import { useReducer } from 'react'
+
+const ACTIONS = {
+  UPDATE_KEYWORD: 'update_keyword',
+  UPDATE_RATING: 'update_rating'
+}
+
+// Add language filter
+// Add button to clear all filters
+
+const ACTION_REDUCERS = {
+  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
+    ...state,
+    keyword: action.payload,
+    times: state.times + 1
+  }),
+  [ACTIONS.UPDATE_RATING]: (state, action) => ({
+    ...state,
+    rating: action.payload
+  })
+}
+
+export default function useForm ({ initialKeyword, initialRating }) {
+  const [state, dispatch] = useReducer(reducer, {
+    keyword: decodeURIComponent(initialKeyword),
+    rating: initialRating,
+    times: 0
+  })
+  const { keyword, times, rating } = state
+  return {
+    keyword,
+    times,
+    rating,
+    updateKeyword: keyword => dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: keyword }),
+    updateRating: keyword => dispatch({ type: ACTIONS.UPDATE_RATING, payload: keyword })
+  }
+}
+
+const reducer = (state, action) => {
+  const actionReducer = ACTION_REDUCERS[action.type]
+  return actionReducer ? actionReducer(state, action) : state
+}
+
+// Common way of writing the reducer function
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case ACTIONS.UPDATE_KEYWORD:
+//       return {
+//         ...state,
+//         keyword: action.payload,
+//         times: state.times + 1
+//       }
+//
+//     case ACTIONS.UPDATE_RATING:
+//       return {
+//         ...state,
+//         rating: action.payload
+//       }
+//     default:
+//       return state
+//       // throw new Error('Action not supported') // Otra opción para controlar edgecases
+//   }
+// }
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,73 +1,20 @@
-import { memo, useReducer } from 'react'
+import { memo } from 'react'
 import { useLocation } from 'wouter'
+import useForm from './hook'
 
 const RATINGS = ['g', 'pg', 'pg-13', 'r']
 
-const ACTIONS = {
-  UPDATE_KEYWORD: 'update_keyword',
-  UPDATE_RATING: 'update_rating'
-}
-
-// Add language filter
-// Add button to clear all filters
-
-const ACTION_REDUCERS = {
-  [ACTIONS.UPDATE_KEYWORD]: (state, action) => ({
-    ...state,
-    keyword: action.payload,
-    times: state.times + 1
-  }),
-  [ACTIONS.UPDATE_RATING]: (state, action) => ({
-    ...state,
-    rating: action.payload
-  })
-}
-
-const reducer = (state, action) => {
-  const actionReducer = ACTION_REDUCERS[action.type]
-  return actionReducer ? actionReducer(state, action) : state
-}
-
-// Common way of writing the reducer function
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case ACTIONS.UPDATE_KEYWORD:
-//       return {
-//         ...state,
-//         keyword: action.payload,
-//         times: state.times + 1
-//       }
-//
-//     case ACTIONS.UPDATE_RATING:
-//       return {
-//         ...state,
-//         rating: action.payload
-//       }
-//     default:
-//       return state
-//       // throw new Error('Action not supported') // Otra opción para controlar edgecases
-//   }
-// }
-
 function SearchForm ({ initialKeyword = '', initialRating = 'g' }) {
-  // const [rating, setRating] = useState(initialRating)
-
-  const [state, dispatch] = useReducer(reducer, {
-    keyword: decodeURIComponent(initialKeyword),
-    rating: initialRating,
-    times: 0
-  })
-
-  const { keyword, times, rating } = state
+  const { keyword, times, rating, updateKeyword, updateRating } = useForm({ initialRating, initialKeyword })
 
   const [, pushLocation] = useLocation()
 
   const handleChange = (evt) => {
-    dispatch({ type: ACTIONS.UPDATE_KEYWORD, payload: evt.target.value })
+    updateKeyword(evt.target.value)
   }
 
   const handleChangeRating = (evt) => {
-    dispatch({ type: ACTIONS.UPDATE_RATING, payload: evt.target.value })
+    updateRating(evt.target.value)
   }
 
   const handleSubmit = (evt) => {
