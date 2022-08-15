8e5012d686d583af395a6d9546faf60fa0c0123e
sergio-dev <sergio-dev@outlook.com>
2022-08-15 17:11:04 +0200

    Build: change setteings to deploy

--- src/services/settings.js
+++ src/services/settings.js
@@ -2,4 +2,5 @@ export const GIPHY_KEY = 'VFEJhtLSFk9qZdOXMUqoWYvAzsv8ZMVM'
 
 export const GIPHY_URL = 'https://api.giphy.com/v1'
 
-export const API_URL = 'https://giffy-api-prod-api-tests-7x25w3.mo2.mogenius.io/api'
+// export const API_URL = 'https://giffy-api-prod-api-tests-7x25w3.mo2.mogenius.io/api'
+export const API_URL = '/api'
